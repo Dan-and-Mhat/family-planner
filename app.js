4625e6b59fbf9922e8b05eb8243eab242eec9fc4
Dan-and-Mhat <danieledicarlo@live.co.uk>
2026-06-02 19:32:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -320,22 +320,35 @@ function useFavourite(item) {
 
     const day =
         prompt(
-            "Which day? (monday, tuesday, wednesday...)"
+            "Choose:\n" +
+            "1 Monday\n" +
+            "2 Tuesday\n" +
+            "3 Wednesday\n" +
+            "4 Thursday\n" +
+            "5 Friday\n" +
+            "6 Saturday\n" +
+            "7 Sunday"
         );
 
-    if (!day) return;
+    const days = [
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+        "sunday"
+    ];
+
+    const selectedDay =
+        days[parseInt(day) - 1];
+
+    if (!selectedDay) return;
 
     const input =
         document.getElementById(
-            day.toLowerCase() + "Meal"
+            selectedDay + "Meal"
         );
-
-    if (!input) {
-
-        alert("Day not found");
-
-        return;
-    }
 
     input.value = meal;
 
